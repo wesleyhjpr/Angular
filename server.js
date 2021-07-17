099063a52b63c093213224cd0702b36bc8b1e564
wesley <wesleyhjpr@gmail.com>
2021-07-17 18:00:33 -0300

    awea

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/angular-wes'));
+app.use(express.static('./dist/angular'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/'}),
+    res.sendFile('index.html', {root: 'dist/angular/'}),
 );
 
 // Start the app by listening on the default Heroku port
